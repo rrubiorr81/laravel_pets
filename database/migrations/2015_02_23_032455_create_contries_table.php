bbcc2c32c26dc9d7f6e59d2d1ccdfa66da8f76a7
Richard Rubio <rrubiorr81@gmail.com>
2015-02-22 22:46:13 -0500

    playing w/ migrations

---
+++ database/migrations/2015_02_23_032455_create_contries_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateContriesTable extends Migration {
+
+	/**
+	 * Run the migrations.
+	 *
+	 * @return void
+	 */
+	public function up()
+	{
+		Schema::create('countries', function(Blueprint $table)
+		{
+			$table->increments('id');
+			$table->string('name');
+			$table->integer('population');
+			$table->timestamps();
+		});
+	}
+
+	/**
+	 * Reverse the migrations.
+	 *
+	 * @return void
+	 */
+	public function down()
+	{
+		Schema::drop('countries');
+	}
+
+}
